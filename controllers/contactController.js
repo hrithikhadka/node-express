b5d0851ae8790eb48257951934f60d660ac07981
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-05 23:44:06 +0545

    built-in middleware for POST Request Body

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -11,6 +11,7 @@ const getContact = (req, res) => {
 //@access public
 
 const createContact = (req, res) => {
+  console.log("The request body is :", req.body);
   res.status(200).json({ message: "Create Contact" });
 };
 
